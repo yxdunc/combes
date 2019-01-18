74e8ba5cf72a47b8c52cd0183893710e051f95e0
Robin Guignard-Perret <yxdunc@users.noreply.github.com>
2019-01-18 01:39:51 +0100

    [Add][Packaging] setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='combes',
+   version='0.0.1',
+   description='',
+   author='Robin Guignard-Perret',
+   author_email='',
+   install_requires=['click'],
+)
