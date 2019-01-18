05cdac3e40c0fe982daa6d5ec1c14eaffeb3a64b
Robin Guignard-Perret <yxdunc@users.noreply.github.com>
2019-01-18 02:37:43 +0100

    [Packaging][Refactoring] converted to module and proper target in setup.py

---
+++ combes/__init__.py
@@ -0,0 +1,5 @@
+from cmd_perm import perm
+from cmd_comb import comb
+from cmd_subl import subl
+from cmd_allp import allp
+from helper_stdin import get_piped_stdin
--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(
    name='combes',
-   version='0.0.1',
+   version='0.0.2',
    description='',
    author='Robin Guignard-Perret',
    author_email='',
+   packages=setuptools.find_packages(),
+   scripts=['bin/combes'],
    install_requires=['click'],
 )
